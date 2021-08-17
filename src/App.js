5aa76dab76e02782e685487d8fa3c05fb8c0b67a
Ana Helena <anahelenarp@hotmail.com>
2021-08-17 10:27:17 -0300

    Add code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css';
+import Advice from './components/Advice';
+
+function App() {
+  return (
+    <div className="App">
+      <Advice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Advice/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import api from '../../api'
+
+const AdviceWrapper = ({ advice }) => {
+    return <div>
+        <h1>{advice}</h1>
+    </div>
+}
+
+const Advice = () => {
+    let [advice, setAdvice] = React.useState(null)
+
+    React.useEffect(() => {
+        api.getData()
+            .then((response) => {
+                setAdvice(response.data.slip.advice)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, []);
+
+
+    return <AdviceWrapper advice={advice} />
+}
+
+export default Advice
